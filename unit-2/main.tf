d53f1d863a2e3a670972a2c2b5b9e80e4eb72a5e
batten <david.batten@packsize.com>
2024-12-29 21:26:39 -0700

    unit 2 done

---
+++ unit-2/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+resource "azurerm_resource_group" "tf_time" {
+  name     = "rg-tf-time"
+  location = "eastus2"
+}
+
+variable "subscription_id" {
+  description = "The ID of your Azure subscription"
+  nullable    = false
+  type        = string
+}
+
+provider "azurerm" {
+  subscription_id = var.subscription_id
+
+  features {}
+}
